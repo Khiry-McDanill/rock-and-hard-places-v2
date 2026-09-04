12c3cb020c372058e220e5046b66004bdd1674c6
khiry <khi.maurice@gmail.com>
2026-09-04 09:26:28 -0400

    Complete RH&P-002 SQLite persistence configuration

--- src/test/java/com/rockandhardplaces/RockAndHardPlacesApplicationTests.java
+++ src/test/java/com/rockandhardplaces/RockAndHardPlacesApplicationTests.java
@@ -1,12 +1,30 @@
 package com.rockandhardplaces;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.sql.Connection;
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RockAndHardPlacesApplicationTests {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void sqliteDataSourceCanOpenConnection() throws Exception {
+        try (Connection connection = dataSource.getConnection()) {
+            assertTrue(connection.isValid(1));
+            assertEquals("SQLite", connection.getMetaData().getDatabaseProductName());
+        }
     }
 }
